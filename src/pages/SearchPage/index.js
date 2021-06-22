ae2e07b2dc663f2fd484ba4ca23f4122121a1a66
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-06-21 22:39:44 -0300

    Adding header logo

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -13,7 +13,10 @@ export default function Index() {
 
   return(
     <div className={styles.SearchPageContainer}>
-      <h1>Anime List</h1>
+      <div className={styles.SearchPageHeader}>
+        <h1>Anime List </h1>
+        <img className={styles.headerImage} src="https://i.pinimg.com/originals/fc/f7/6a/fcf76aaac5ba83de8c968c2d4371ac58.png" alt="" />
+      </div>
       <div className={styles.searchInputContainer}>
         <input 
           className={styles.searchInput}
